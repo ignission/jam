3a0570bad3ed040b866587c3c545bafc0185f40c
Shoma Nishitateno <shoma416@gmail.com>
2020-04-19 23:07:37 +0900

    update: configure webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: `${__dirname}/src/index.tsx`,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        // 追記
+        test: /\.(jpg|png)$/,
+        loaders: 'url-loader'
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    modules: ['node_modules', path.resolve(__dirname, 'src')],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: `${__dirname}/public`,
+    port: '3000',
+    historyApiFallback: { disableDotRule: true },
+    watchOptions: {
+      poll: 1000,
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/public/index.html`,
+    }),
+  ],
+};
