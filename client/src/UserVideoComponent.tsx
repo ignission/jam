0dddaba7ff21fff97f23261be99b51c8e54610a8
Shoma Nishitateno <shoma416@gmail.com>
2020-04-20 00:42:45 +0900

    wip: UserVideoComponent

---
+++ client/src/UserVideoComponent.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import OpenViduVideoComponent from 'OvVideo';
+import { StreamManager } from 'openvidu-browser';
+import './UserVideo.css';
+
+interface Props {
+  streamManager: StreamManager;
+}
+
+const UserVideoComponent: React.FC<Props> = (props) => {
+  const getNicknameTag = () => {
+    // Gets the nickName of the user
+    return JSON.parse(this.props.streamManager.stream.connection.data)
+      .clientData;
+  };
+
+  return (
+    <div>
+      {this.props.streamManager !== undefined ? (
+        <div className="streamcomponent">
+          <OpenViduVideoComponent streamManager={props.streamManager} />
+          <div>
+            <p>{getNicknameTag()}</p>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default UserVideoComponent;
+
+/**
+export default class UserVideoComponent extends Component {
+
+    getNicknameTag() {
+        // Gets the nickName of the user
+        return JSON.parse(this.props.streamManager.stream.connection.data).clientData;
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.streamManager !== undefined ? (
+                    <div className="streamcomponent">
+                        <OpenViduVideoComponent streamManager={this.props.streamManager} />
+                        <div><p>{this.getNicknameTag()}</p></div>
+                    </div>
+                ) : null}
+            </div>
+        );
+    }
+}
+ */
