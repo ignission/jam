2a1c8be5fa03b24b5e7624d4349330a3a02522e9
Shoma Nishitateno <shoma416@gmail.com>
2020-04-27 01:42:44 +0900

    clean: rename

--- emoi-client/src/index.tsx
+++ emoi-client/src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'sanitize.css';
-import './styles.css';
+import './GlobalStyles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
