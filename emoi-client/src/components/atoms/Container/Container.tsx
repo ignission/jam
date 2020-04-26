3d7c171d3c9553d78cd68aab80e5fac05cb7701f
Shoma Nishitateno <shoma416@gmail.com>
2020-04-27 01:41:50 +0900

    fix: home design

--- emoi-client/src/components/atoms/Container/Container.tsx
+++ emoi-client/src/components/atoms/Container/Container.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Container.css';
 
 export const Container: React.FC = ({ children }) => (
   <div className="container">{children}</div>
--- emoi-client/src/index.tsx
+++ emoi-client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import 'sanitize.css';
+import './styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
