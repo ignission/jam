1e0d2e3239cb89512bfd290e12fd2d974df1d64e
iwa (Yuki YOSHIIWA) <yuki.yoshiiwa@nulab.com>
2020-05-03 22:49:12 +0900

    feat: enable code-splitting, remove unused libraries

---
+++ emoi-client/webpack.dev.js
@@ -0,0 +1,15 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: `${__dirname}/public`,
+    port: '3000',
+    historyApiFallback: { disableDotRule: true },
+    watchOptions: {
+      poll: 1000,
+    },
+  },
+});
