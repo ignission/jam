b364b192b2d5a5c4ff4571de1225f93e9aafb859
Shoma Nishitateno <shoma416@gmail.com>
2020-05-12 04:06:51 +0900

    fix: compile error

--- emoi-server/src/main/scala/emoi/server/infrastructure/persistence/mysql/ops/BaseTableOps.scala
+++ emoi-server/src/main/scala/emoi/server/infrastructure/persistence/mysql/ops/BaseTableOps.scala
@@ -9,6 +9,8 @@ import slick.jdbc.SQLiteProfile.api._
 
 trait BaseTableOps[A <: Entity[A], Table <: BaseTable[A]] {
 
+  import emoi.server.infrastructure.persistence.mysql.tables.JdbcMapper._
+
   protected val tableQuery: TableQuery[Table]
 
   lazy val createTable = tableQuery.schema.create
@@ -17,10 +19,10 @@ trait BaseTableOps[A <: Entity[A], Table <: BaseTable[A]] {
     tableQuery.result
 
   def select(id: Id[A]): DBIORead[Option[A]] =
-    tableQuery.filter(_.id === id.value).result.headOption
+    tableQuery.filter(_.id === id).result.headOption
 
-  def write(obj: Entity[A]): DBIOWrite = ???
-    // tableQuery.filter(_.id.value === obj.id.value).insertOrUpdate(obj)
+  def write(obj: A): DBIOWrite =
+    tableQuery.filter(_.id === obj.id).insertOrUpdate(obj)
 
   def write(objs: Seq[A]): DBIOWrites = {
     DBIO.sequence(
