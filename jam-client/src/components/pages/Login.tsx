0f6b5d4d83f44a105109a2f757d4e97203e1897f
makkkkky <fukudadada@gmail.com>
2020-05-26 13:51:14 +0900

    make login page by material ui

--- jam-client/src/components/pages/Login.tsx
+++ jam-client/src/components/pages/Login.tsx
@@ -1,5 +1,18 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import TextField from '@material-ui/core/TextField';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        // width: 200,
+      },
+    },
+  })
+);
 
 const Base = styled.div({
   background: '#af62a4',
@@ -10,21 +23,39 @@ const Base = styled.div({
 });
 
 const Box = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
   width: '100%',
-  height: '100%',
+  // height: '100%',
+  maxWidth: 600,
+  // background: '#fff',
   padding: 80,
 });
 
 const Contents = styled.div({
-  background: '#fff',
-  height: '100%',
+  // background: '#fff',
+  maxWidth: 400,
+  // height: '100%',
 });
 
 const View: React.FC = () => {
+  const classes = useStyles();
   return (
     <Base>
       <Box>
-        <Contents>aaa</Contents>
+        <Contents>
+          <form className={classes.root} noValidate autoComplete="off">
+            <TextField label="ID" type="text" variant="filled" fullWidth />
+            <TextField
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              variant="filled"
+              fullWidth
+            />
+          </form>
+        </Contents>
       </Box>
     </Base>
   );
