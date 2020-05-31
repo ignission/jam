f1dacf0a35b2735e17b4a8815d4b79b1d1b0f0ee
Shoma Nishitateno <shoma416@gmail.com>
2020-06-01 00:04:21 +0900

    clean: refactor package in test

--- jam-server/jam-server/src/test/scala/e2e/ApiRouteSpec.scala
+++ jam-server/jam-server/src/test/scala/e2e/ApiRouteSpec.scala
@@ -1,4 +1,4 @@
-package unit.rest.routes
+package e2e
 
 import java.time.{Instant, ZoneId, ZonedDateTime}
 
@@ -24,13 +24,7 @@ import jam.shared.WithDatabase
 import tech.ignission.openvidu4s.akka.interpreters.OpenViduHttpDSLOnAkka
 import tech.ignission.openvidu4s.core.Basic
 import tech.ignission.openvidu4s.core.apis.AllAPI
-import tech.ignission.openvidu4s.core.datas.{
-  GeneratedToken,
-  InitializedSession,
-  Session,
-  SessionId,
-  Token
-}
+import tech.ignission.openvidu4s.core.datas._
 
 class ApiRouteSpec extends AnyWordSpec with Matchers with ScalatestRouteTest with WithDatabase {
 
