0169785ab59a18a76c51c748e8066e03addb8196
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2021-05-25 19:10:19 +0900

    feat: prod config

--- jam-client/webpack.prod.js
+++ jam-client/webpack.prod.js
@@ -1,6 +1,12 @@
 const { merge } = require('webpack-merge');
+const webpack = require("webpack")
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new webpack.DefinePlugin({
+      'WS_URL': JSON.stringify('ws://jam.ignission.tech')
+    })
+  ],
 });
