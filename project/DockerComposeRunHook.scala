b9bf04d167978002e4947f01dc9ecc48db1c89d0
Shoma Nishitateno <shoma416@gmail.com>
2021-05-27 23:39:20 +0900

    chore: upgrade openvidu kms

--- project/DockerComposeRunHook.scala
+++ project/DockerComposeRunHook.scala
@@ -7,7 +7,7 @@ object DockerComposeRunHook {
   def apply(base: File): PlayRunHook = {
     object DockerCompose extends PlayRunHook {
       override def beforeStarted(): Unit = {
-        Process("docker compose up -d redis", base).!
+        Process("docker compose up -d redis kms", base).!
       }
       override def afterStopped(): Unit = {
         Process("docker-compose down", base).!
