ec5a17076d7361caf213426800549770187f896c
Shoma Nishitateno <shoma416@gmail.com>
2021-06-14 23:22:26 +0900

    feat: support post

--- jam-infrastructure/src/main/scala/jam/infrastructure/http/HttpDSL.scala
+++ jam-infrastructure/src/main/scala/jam/infrastructure/http/HttpDSL.scala
@@ -27,6 +27,12 @@ object HttpDSL {
 
   def get[A, R: _http](param: HttpParam)(implicit decoder: Decoder[A]): Eff[R, A] =
     Eff.send[HttpDSL, R, A](Get(param, decoder))
+
+  def post[A1, A2, R: _http](param: HttpParam, data: A1)(implicit
+      encoder: Encoder[A1],
+      decoder: Decoder[A2]
+  ): Eff[R, A2] =
+    Eff.send[HttpDSL, R, A2](Post(param, data, encoder, decoder))
 }
 
 object HttpInterpreter {
