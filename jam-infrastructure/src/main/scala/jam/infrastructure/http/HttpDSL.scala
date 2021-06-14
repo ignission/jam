52efdadd12f063641738919154111435c9ae5ac5
Shoma Nishitateno <shoma416@gmail.com>
2021-06-14 23:00:48 +0900

    wip: decode (get)

--- jam-infrastructure/src/main/scala/jam/infrastructure/http/HttpDSL.scala
+++ jam-infrastructure/src/main/scala/jam/infrastructure/http/HttpDSL.scala
@@ -1,9 +1,11 @@
 package jam.infrastructure.http
 
+import io.circe._
 import org.atnos.eff.Interpret.translate
 import org.atnos.eff._
 import org.atnos.eff.all.fromEither
 import sttp.client3._
+import sttp.client3.circe._
 
 case class HttpParam(url: String, credential: Credential)
 
@@ -11,18 +13,19 @@ sealed trait Credential
 case class BasicCredential(username: String, password: String) extends Credential
 
 sealed trait HttpDSL[+A]
-case class Get(param: HttpParam) extends HttpDSL[String]
+case class Get[A](param: HttpParam, implicit val decoder: Decoder[A]) extends HttpDSL[A]
+case class Post(param: HttpParam)                                     extends HttpDSL[String]
 
 object HttpDSL {
   type Stack    = Fx.fx2[HttpDSL, Either[String, *]]
   type _http[R] = HttpDSL |= R
 
-  def get[A, R: _http](param: HttpParam): Eff[R, String] =
-    Eff.send[HttpDSL, R, String](Get(param))
+  def get[A, R: _http](param: HttpParam)(implicit decoder: Decoder[A]): Eff[R, A] =
+    Eff.send[HttpDSL, R, A](Get(param, decoder))
 }
 
 object HttpInterpreter {
-  type _either[R] = Either[String, *] |= R
+  type _either[R] = Either[ResponseException[String, Error], *] |= R
 
   def run[R, U, A](
       effects: Eff[R, A]
@@ -32,18 +35,24 @@ object HttpInterpreter {
 
       override def apply[X](kv: HttpDSL[X]): Eff[U, X] =
         kv match {
-          case Get(param) =>
-            val res = buildGetRequest(param).send(backend)
+          case Get(param, decoder) =>
+            val res = buildGetRequest(param)(decoder).send(backend)
             fromEither(res.body)
+          case Post(_) => ???
+        }
+
+      private def buildGetRequest[T](
+          param: HttpParam
+      )(implicit
+          decoder: Decoder[T]
+      ): RequestT[Identity, Either[ResponseException[String, Error], T], Any] =
+        buildRequest(param).get(uri"${param.url}").response(asJson[T])
+
+      private def buildRequest(param: HttpParam): RequestT[Empty, Either[String, String], Any] =
+        param.credential match {
+          case BasicCredential(username, password) =>
+            basicRequest.auth.basic(user = username, password = password)
         }
     })
 
-  private def buildGetRequest(param: HttpParam): RequestT[Identity, Either[String, String], Any] =
-    buildRequest(param).get(uri"${param.url}").response(asString)
-
-  private def buildRequest(param: HttpParam): RequestT[Empty, Either[String, String], Any] =
-    param.credential match {
-      case BasicCredential(username, password) =>
-        basicRequest.auth.basic(user = username, password = password)
-    }
 }
