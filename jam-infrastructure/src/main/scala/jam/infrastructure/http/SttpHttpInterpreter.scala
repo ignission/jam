001b4bba28275ac3bb3f4bc2dc7508a489b98ed5
Shoma Nishitateno <shoma416@gmail.com>
2021-06-15 22:23:46 +0900

    refactor: separate interpreter

---
+++ jam-infrastructure/src/main/scala/jam/infrastructure/http/SttpHttpInterpreter.scala
@@ -0,0 +1,48 @@
+package jam.infrastructure.http
+
+import io.circe._
+import org.atnos.eff.Interpret.translate
+import org.atnos.eff._
+import org.atnos.eff.all.fromEither
+import sttp.client3.circe._
+import sttp.client3.{ResponseException, _}
+
+object SttpHttpInterpreter {
+  type _either[R] = Either[ResponseException[String, Error], *] |= R
+
+  def run[R, U, A](
+      effects: Eff[R, A]
+  )(implicit m: Member.Aux[HttpDSL, R, U], either: _either[U]): Eff[U, A] =
+    translate(effects)(new Translate[HttpDSL, U] {
+      private val backend = HttpURLConnectionBackend()
+
+      override def apply[X](kv: HttpDSL[X]): Eff[U, X] = {
+        val result = kv match {
+          case Get(param, decoder) =>
+            get(param)(decoder).send(backend).body
+          case Post(param, data, encoder, decoder) =>
+            post(param, data)(encoder, decoder).send(backend).body
+        }
+        fromEither(result)
+      }
+
+      private def get[T](
+          param: HttpParam
+      )(implicit
+          decoder: Decoder[T]
+      ): RequestT[Identity, Either[ResponseException[String, Error], T], Any] =
+        setCredential(param).get(uri"${param.url}").response(asJson[T])
+
+      private def post[T1, T2](param: HttpParam, data: T1)(implicit
+          encoder: Encoder[T1],
+          decoder: Decoder[T2]
+      ): RequestT[Identity, Either[ResponseException[String, Error], T2], Any] =
+        setCredential(param).post(uri"${param.url}").body(data).response(asJson[T2])
+
+      private def setCredential(param: HttpParam): RequestT[Empty, Either[String, String], Any] =
+        param.credential match {
+          case BasicCredential(username, password) =>
+            basicRequest.auth.basic(user = username, password = password)
+        }
+    })
+}
